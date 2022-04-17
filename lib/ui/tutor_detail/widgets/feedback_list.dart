a47f52dfaeeb4ee07bdd845cc485ad726c833a9f
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2022-04-17 22:40:10 +0700

    Add error and empty page to Tutor Feedback

--- lib/ui/tutor_detail/widgets/feedback_list.dart
+++ lib/ui/tutor_detail/widgets/feedback_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:leet_tutur/models/user_feedback.dart';
 import 'package:leet_tutur/ui/tutor_detail/widgets/tutor_review.dart';
+import 'package:leet_tutur/widgets/empty_page.dart';
 
 class FeedbackList extends StatefulWidget {
   final List<UserFeedback> userFeedbacks;
@@ -15,7 +16,7 @@ class _FeedbackListState extends State<FeedbackList> {
   @override
   Widget build(BuildContext context) {
     var feedbacks = widget.userFeedbacks;
-    return ListView.builder(
+    return feedbacks.isNotEmpty ? ListView.builder(
       primary: false,
       shrinkWrap: true,
       itemBuilder: (context, index) {
@@ -24,6 +25,6 @@ class _FeedbackListState extends State<FeedbackList> {
         return TutorReview(userFeedback: userFeedback);
       },
       itemCount: feedbacks.length,
-    );
+    ) : const EmptyPage();
   }
 }
