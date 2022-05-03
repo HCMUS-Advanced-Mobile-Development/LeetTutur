48fc95609b9dddb285c728042d9fbdad7cefa206
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2022-05-03 19:34:01 +0700

    App logo

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"leet_tutur", origin, size)) {
+  if (!window.CreateAndShow(L"Leet Tutur", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
