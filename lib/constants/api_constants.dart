e3fd6e7d52665fd7d493b04eafad0f7a7f31f69b
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2022-05-08 22:42:04 +0700

    Env save

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-    static final String baseURL = dotenv.env['BASE_URL']!;
-    static final String meetingURL = dotenv.env['MEETING_URL']!;
+  static final String baseURL =
+      dotenv.env['BASE_URL'] ?? "https://sandbox.api.lettutor.com";
+  static final String meetingURL =
+      dotenv.env['MEETING_URL'] ?? "https://meet.lettutor.com";
 }
