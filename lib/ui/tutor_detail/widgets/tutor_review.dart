e2dd0b502f4922c39d79d7b378c8d6d82c76e604
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2022-05-16 22:08:25 +0700

    Enhance user feedback

--- lib/ui/tutor_detail/widgets/tutor_review.dart
+++ lib/ui/tutor_detail/widgets/tutor_review.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:leet_tutur/models/user_feedback.dart';
+import 'package:leet_tutur/utils/date_time_utils.dart';
 
 class TutorReview extends StatefulWidget {
   final UserFeedback userFeedback;
@@ -11,7 +12,6 @@ class TutorReview extends StatefulWidget {
 }
 
 class _TutorReviewState extends State<TutorReview> {
-
   @override
   Widget build(BuildContext context) {
     final userFeedback = widget.userFeedback;
@@ -23,8 +23,42 @@ class _TutorReviewState extends State<TutorReview> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(userFeedback.firstInfo?.name ?? "Unknown", style: Theme.of(context).textTheme.bodyMedium),
-              Text(userFeedback.content ?? "", style: Theme.of(context).textTheme.caption)
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage:
+                        Image.network(userFeedback.firstInfo?.avatar ?? "")
+                            .image,
+                  ),
+                  const SizedBox(width: 8,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            userFeedback.firstInfo?.name ?? "Unknown",
+                          ),
+                          Text(" - ${userFeedback.rating}"),
+                        ],
+                      ),
+                      Text(
+                        DateTimeUtils.humanize(
+                            DateTime.parse(userFeedback.createdAt ?? "")),
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                userFeedback.content ?? "",
+                style: Theme.of(context).textTheme.bodyLarge,
+              )
             ],
           ),
         ),
